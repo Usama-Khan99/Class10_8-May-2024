a8e4f6e751cc2f396d8d71b91ddab1d454e895f7
Usama-Khan99 <65035298+Usama-Khan99@users.noreply.github.com>
2024-05-08 19:43:25 +0500

    Switch Case

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+// turnary operator (Other method of if else statement in a single line)
+Object.defineProperty(exports, "__esModule", { value: true });
+let isHungry = true;
+let snack = isHungry ? "apple" : "water";
+console.log(`You should have some ${snack}.`);
